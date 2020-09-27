ed65735d07a54d1b41551fa48d747a195127e3db
Michal.Czernikowski <czernik44@gmail.com>
2020-09-27 17:55:32 +0200

    Simple map app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, {Component} from 'react';
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 import './App.css';
 import * as vehiclesData from './data/vehicles.json'
+import {Icon} from "leaflet";
+
+const busIcon = new Icon({
+    iconUrl:'/vehicleIcon.svg',
+    iconSize:[25,25]
+})
 
 export default function App() {
+    const [activeVehicle, setActiveVehicle] = React.useState(null);
     return <Map center={[50.049683, 19.944544]} zoom={13}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -13,12 +20,31 @@ export default function App() {
             <Marker
                 key={vehicle.id}
                 position={[
-                    vehicle.latitude/3600000,
-                    vehicle.longitude/3600000
+                    vehicle.latitude / 3600000,
+                    vehicle.longitude / 3600000
                 ]}
+                onClick={() => {
+                    setActiveVehicle(vehicle);
+                }}
+                icon = {busIcon}
             />
-
         ))}
+        {activeVehicle && (
+            <Popup
+                position={[
+                    activeVehicle.latitude / 3600000,
+                    activeVehicle.longitude / 3600000
+                ]}
+                onClose={()=>{
+                    setActiveVehicle(null);
+                }}
+            >
+                <div>
+                    <h4>{activeVehicle.name}</h4>
+                    <p>{activeVehicle.category}</p>
+                </div>
+            </Popup>
+        )}
     </Map>;
 
 
