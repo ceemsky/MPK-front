0ddd5794155e0d28e2e75cc0cd474bbeba1a8610
Michal.Czernikowski <czernik44@gmail.com>
2020-09-28 23:17:03 +0200

    added data cleaning mockup added data handling class

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, {Component} from 'react';
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 import './App.css';
-import * as vehiclesData from './data/vehicles.json'
 import {Icon} from "leaflet";
-
+import DataExtractor from './utils/DataExtractor';
 const busIcon = new Icon({
     iconUrl:'/vehicleIcon.svg',
     iconSize:[25,25]
 })
 
+
 export default function App() {
+
+    const dataExtractor = new DataExtractor(true);
+    const vehicles = dataExtractor.getData();
     const [activeVehicle, setActiveVehicle] = React.useState(null);
     return <Map center={[50.049683, 19.944544]} zoom={13}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {vehiclesData.vehicles.map(vehicle => (
+        {vehicles.map(vehicle => (
             <Marker
                 key={vehicle.id}
                 position={[
